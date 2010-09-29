22c08662405705194497c245a9dbd69ac0698a4c
Hasan Karahan <hasan.karahan81@gmail.com>
2010-09-29 23:39:00 +0000

    DB backend

--- starwars/home/models.py
+++ starwars/home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Gallery (models.Model):
+
+    class Meta:
+
+        verbose_name_plural = 'galleries'
+
+    name = models.CharField (max_length=256)
+    url = models.URLField (max_length=256, verify_exists=False)
+    ignore = models.BooleanField (default=False)
+
+    COLOR_CHOICES = (
+        ('red'   , 'red'),
+        ('green' , 'green'),
+        ('blue'  , 'blue'),
+    )
+
+    color = models.CharField (max_length=5, choices=COLOR_CHOICES)
+
+    def __unicode__(self):
+        return "%s" % self.name
+
+class Image (models.Model):
+
+    name = models.CharField (max_length=256)
+    url = models.URLField (max_length=256, verify_exists=False)
+    ignore = models.BooleanField (default=False)
+
+    gallery = models.ForeignKey (Gallery)
+    rate = models.DecimalField (max_digits=4, decimal_places=2)
+
+    def __unicode__(self):
+        return "%s" % self.name
--- starwars/home/urls.py
+++ starwars/home/urls.py
@@ -1,13 +1,27 @@
-from django.conf               import settings
+from django.conf import settings
 from django.conf.urls.defaults import *
+from home.views import *
+from home.models import *
 
 urlpatterns = patterns('django.views.generic.simple',
 
-    url (
-        r'^$', 'direct_to_template', {
-            'template': 'index.html',
-            'extra_context': {}
-        }, name="master"
+ ## url (
+ ##     r'^$', 'direct_to_template', {
+ ##         'template': 'index.html',
+ ##         'extra_context': {'galleries': Gallery.objects.all()}
+ ##     }, name="master"
+ ## ),
+
+    url(
+        r'^$',
+        galleries_all,
+        name="galleries_all"
+    ),
+
+    url(
+        r'^(?P<id>\d+)/$',
+        galleries_by_episode,
+        name="galleries_by_episode"
     ),
 
 )
